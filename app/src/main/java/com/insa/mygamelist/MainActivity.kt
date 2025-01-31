d0ae1de0b41988bc002229110b70d5a0008fdf37
LarveTable <cyorick@gmail.com>
2025-01-31 17:33:04 +0100

    tp1 completed

--- app/src/main/java/com/insa/mygamelist/MainActivity.kt
+++ app/src/main/java/com/insa/mygamelist/MainActivity.kt
@@ -4,15 +4,43 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import com.insa.mygamelist.data.Cover
+import com.insa.mygamelist.data.Games
 import com.insa.mygamelist.data.IGDB
 import com.insa.mygamelist.ui.theme.MyGamesListTheme
 
@@ -27,7 +55,6 @@ class MainActivity : ComponentActivity() {
 
         enableEdgeToEdge()
         setContent {
-
             MyGamesListTheme {
                 Scaffold(topBar = {
                     TopAppBar(colors = topAppBarColors(
@@ -35,11 +62,98 @@ class MainActivity : ComponentActivity() {
                         titleContentColor = Color.Black,
                     ), title = { Text("My Games List") })
                 }, modifier = Modifier.fillMaxSize()) { innerPadding ->
-
-                    Text("À remplir", modifier = Modifier.padding(innerPadding))
-
+                    DisplayAllGameCells(innerPadding, IGDB)
                 }
             }
         }
+    }
+
+    val getPossibleCover: (List<Cover>, Long) -> (Cover?) = {
+        liste, id ->
+            liste.find { co ->
+                co.id == id
+            }
+    }
+
+    @Composable
+    fun DisplayAllGameCells(innerPadding: PaddingValues, igdb: IGDB) : Unit {
+        val coverModifier = getCoverModifier()
+        val boxModifier = getBoxModifier()
+        LazyColumn (modifier=Modifier.padding(innerPadding)) {
+            items(igdb.games) { game ->
+                val foundCover: Cover? = getPossibleCover(igdb.covers, game.cover)
+                    Row {
+                        foundCover?.let { /* If a cover is found, display it */
+                            AsyncImage(
+                                model = "https:" + foundCover.url, /* Add "https:" since it's not present in the JSON file */
+                                contentDescription = null,
+                                modifier = coverModifier
+                            )
+                        } ?: run { /* Display "Missing" if cover not found (for fun) */
+                            Image(
+                                painter = painterResource(R.drawable.missing),
+                                contentDescription = "Contact profile picture",
+                                modifier = coverModifier
+                            )
+                        }
+                        Box(
+                            modifier = boxModifier
+                        ) {
+                            Column (modifier = Modifier.align(Alignment.CenterStart)) {
+                                Text(
+                                    text = game.name,
+                                    fontWeight = FontWeight.Bold,
+                                    textDecoration = TextDecoration.Underline,
+                                )
+                                Text(
+                                    text = "Genres : "+game.genres.toString(),
+                                )
+                            }
+                        }
+                        /*Text(text = game.name)
+                        Text(text= "Genres : ")*/
+                    }
+            }
+        }
+    }
+
+    @Composable
+    fun getCoverModifier() : Modifier {
+        val rainbowColorsBrush = remember { /* Colorful border for images. For cosmetic purposes only ! */
+            Brush.sweepGradient(
+                listOf(
+                    Color(0xFF9575CD),
+                    Color(0xFFBA68C8),
+                    Color(0xFFE57373),
+                    Color(0xFFFFB74D),
+                    Color(0xFFFFF176),
+                    Color(0xFFAED581),
+                    Color(0xFF4DD0E1),
+                    Color(0xFF9575CD)
+                )
+            )
+        }
+
+        val coverModifier = Modifier
+            .padding(4.dp)
+            .height(100.dp)
+            .width(80.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .border(
+                BorderStroke(4.dp, rainbowColorsBrush),
+                RoundedCornerShape(8.dp)
+            )
+
+        return coverModifier
+    }
+
+    fun getBoxModifier(): Modifier {
+        val boxModifier = Modifier
+            .padding(top=4.dp, end=4.dp)
+            .height(100.dp)
+            .background(Color.LightGray)
+            .fillMaxWidth()
+
+        return boxModifier
     }
 }
--- app/src/main/java/com/insa/mygamelist/data/IGDB.kt
+++ app/src/main/java/com/insa/mygamelist/data/IGDB.kt
@@ -8,15 +8,50 @@ import com.insa.mygamelist.R
 object IGDB {
 
     lateinit var covers: List<Cover>
+    lateinit var games: List<Games>
+    lateinit var genres: List<Genre>
+    lateinit var logos: List<Logo>
+    lateinit var platforms: List<Platform>
 
     fun load(context: Context) {
         val coversFromJson: List<Cover> = Gson().fromJson(
             context.resources.openRawResource(R.raw.covers).bufferedReader(),
             object : TypeToken<List<Cover>>() {}.type
         )
+        val gamesFromJson: List<Games> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.games).bufferedReader(),
+            object : TypeToken<List<Games>>() {}.type
+        )
+        val genreFromJson: List<Genre> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.genres).bufferedReader(),
+            object : TypeToken<List<Genre>>() {}.type
+        )
+        val logoFromJson: List<Logo> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.platform_logos).bufferedReader(),
+            object : TypeToken<List<Logo>>() {}.type
+        )
+        val platformFromJson: List<Platform> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.platforms).bufferedReader(),
+            object : TypeToken<List<Platform>>() {}.type
+        )
 
         covers = coversFromJson
+        games = gamesFromJson
+        genres = genreFromJson
+        logos = logoFromJson
+        platforms = platformFromJson
     }
 }
 
 data class Cover(val id: Long, val url: String)
+data class Games(val id: Long,
+                 val cover: Long,
+                val first_release_date: Long,
+                val genres: List<Int>,
+                val name: String,
+                val platforms: List<Int>,
+                val summary: String,
+                val total_rating: Float)
+data class Genre(val id: Long, val name: String)
+data class Logo(val id: Long, val url: String)
+data class Platform(val id: Long, val name: String, val platform_logo: Long)
