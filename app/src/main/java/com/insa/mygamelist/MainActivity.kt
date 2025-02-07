0715311fc0f851d90dd428968e07821916b81958
LarveTable <cyorick@gmail.com>
2025-02-07 15:41:23 +0100

    tp3 completed

--- app/src/main/java/com/insa/mygamelist/MainActivity.kt
+++ app/src/main/java/com/insa/mygamelist/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -22,12 +23,17 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -42,6 +48,7 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import coil3.compose.AsyncImage
 import com.insa.mygamelist.data.Cover
 import com.insa.mygamelist.data.IGDB
@@ -55,7 +62,7 @@ class MainActivity : ComponentActivity() {
     object Home
     // Define a profile route that takes a game id as a parameter
     @Serializable
-    data class GamePage(val id: Long)
+    data class GamePage(val id: Long, val name: String)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -65,20 +72,50 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyGamesListTheme {
+                val title = remember { /* Title change, using remember to allow for redraw */
+                    mutableStateOf("My Games List")
+                }
+                val back = remember { /* To display or not to display the back button */
+                    mutableStateOf(false)
+                }
+                var backAction = remember { /* Action to perform */
+                    {}
+                }
                 Scaffold(topBar = {
                     TopAppBar(colors = topAppBarColors(
                         containerColor = Color.Magenta,
                         titleContentColor = Color.Black,
-                    ), title = { Text("My Games List") })
+                    ), title = { Text(text=title.value) },
+                        navigationIcon = {
+                            if (back.value) {
+                                IconButton(onClick = backAction) {
+                                    Icon(
+                                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                        contentDescription = "Localized description"
+                                    )
+                                }
+                            }
+                        }
+                    )
                 }, modifier = Modifier.fillMaxSize()) { innerPadding ->
                     val navController = rememberNavController()
                     NavHost(navController, startDestination = Home) {
                         composable<Home> {
-                            DisplayAllGameCells(innerPadding, IGDB)
+                            title.value = "My Games List"
+                            back.value = false
+                            DisplayAllGameCells(innerPadding, IGDB, onNavigateToGamePage = { id : Long, name : String ->
+                                navController.navigate(GamePage(id, name)) /* On passe une fonction à la page principale pour naviguer sur les pages des jeux */
+                            } )
                         }
-                        /*composable(GamePage.serializer()) { page ->
-                            DisplayGamePage(page.id, innerPadding, IGDB)
-                        }*/
+                        composable<GamePage> { backStackEntry ->
+                            val game : GamePage = backStackEntry.toRoute()
+                            title.value = game.name
+                            back.value = true
+                            backAction = {
+                                navController.popBackStack()
+                            }
+                            DisplayGamePage(game.id, innerPadding, IGDB)
+                        }
                     }
                 }
             }
@@ -93,7 +130,12 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun DisplayAllGameCells(innerPadding: PaddingValues, igdb: IGDB) : Unit {
+    fun DisplayGamePage(id : Long, innerPadding: PaddingValues, igdb: IGDB) : Unit {
+        Text("Game page for game $id", modifier = Modifier.padding(innerPadding))
+    }
+
+    @Composable
+    fun DisplayAllGameCells(innerPadding: PaddingValues, igdb: IGDB, onNavigateToGamePage: (Long, String) -> Unit ) : Unit {
         val coverModifier = Modifier.getCoverModifier()
         val boxModifier = Modifier.getBoxModifier()
         LazyColumn (modifier=Modifier.padding(innerPadding)) {
@@ -114,7 +156,7 @@ class MainActivity : ComponentActivity() {
                             )
                         }
                         Box(
-                            modifier = boxModifier
+                            modifier = boxModifier.clickable(onClick = { onNavigateToGamePage(game.id, game.name) })
                         ) {
                             Column (modifier = Modifier.align(Alignment.CenterStart)) {
                                 Text(
@@ -181,12 +223,13 @@ class MainActivity : ComponentActivity() {
 
     fun Modifier.getBoxModifier(): Modifier {
         val boxModifier = Modifier
-            .padding(top=4.dp, end=4.dp, start = 4.dp)
+            .padding(top = 4.dp, end = 4.dp, start = 4.dp)
             .height(100.dp)
             .border(
                 BorderStroke(1.dp, Color.Black),
                 RoundedCornerShape(8.dp),
-            ).padding(4.dp)
+            )
+            .padding(4.dp)
             .fillMaxWidth()
 
         return then(boxModifier)
